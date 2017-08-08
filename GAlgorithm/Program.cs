3e4cb80e34b957b2dffc396919ff0ed182ea7c59
lzhyubai@hotmail.com <123456a>
2017-08-08 16:28:12 +0800

    create project

---
+++ GAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GAlgorithm
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
